085dfd2df771f43155ab5ad6ebc34aa8c0b16623
sargaskim <2103702@talnet.nl>
2019-11-25 16:12:21 +0100

    consitions opdr 5

--- conditions_opdrachten/conditions_opdracht_5/js/script.js
+++ conditions_opdrachten/conditions_opdracht_5/js/script.js
@@ -1,1 +1,19 @@
-//javascript code
+function rekenUit(myOperator)
+{
+    var myNumber1=parseInt(document.getElementById("myNumber1").value);
+    var myNumber2=parseInt(document.getElementById("myNumber2").value);
+    var myUitkomst;
+
+    if(myOperator=="+"){
+        myUitkomst = myNumber1 + myNumber2;
+    }
+    else if(myOperator=="-"){
+        myUitkomst = myNumber1 - myNumber2;
+    }
+    else{
+
+    }
+
+    document.getElementById("uitkomst").innerHTML = myNumber1 + myOperator + myNumber2 + " = " + myUitkomst;
+
+}
