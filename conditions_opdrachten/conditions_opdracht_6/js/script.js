f96ba685961e57b2effbf216b0acc30c3078e924
sargaskim <2103702@talnet.nl>
2019-11-25 20:30:35 +0100

    conditions opdr 6

--- conditions_opdrachten/conditions_opdracht_6/js/script.js
+++ conditions_opdrachten/conditions_opdracht_6/js/script.js
@@ -1,1 +1,52 @@
-//javascript code
+function rekenUitP(myOperator)
+{
+    var myNumber1=parseInt(document.getElementById("myNumber1").value);
+    var myNumber2=parseInt(document.getElementById("myNumber2").value);
+    var myUitkomst;
+
+    if(myOperator=="+"){
+        myUitkomst = myNumber1 + myNumber2;
+    }
+   
+    document.getElementById("uitkomst").innerHTML = myNumber1 + myOperator + myNumber2 + " = " + myUitkomst;
+}
+
+function rekenUitM(myOperator)
+{
+    var myNumber1=parseInt(document.getElementById("myNumber1").value);
+    var myNumber2=parseInt(document.getElementById("myNumber2").value);
+    var myUitkomst;
+
+    if(myOperator=="-"){
+        myUitkomst = myNumber1 - myNumber2;
+    }
+        
+    document.getElementById("uitkomst").innerHTML = myNumber1 + myOperator + myNumber2 + " = " + myUitkomst;
+}
+
+function rekenUitK(myOperator)
+{
+    var myNumber1=parseInt(document.getElementById("myNumber1").value);
+    var myNumber2=parseInt(document.getElementById("myNumber2").value);
+    var myUitkomst;
+
+    if(myOperator=="*"){
+        myUitkomst = myNumber1 * myNumber2;
+    }
+   
+    document.getElementById("uitkomst").innerHTML = myNumber1 + myOperator + myNumber2 + " = " + myUitkomst;
+}
+
+function rekenUitD(myOperator)
+{
+    var myNumber1=parseInt(document.getElementById("myNumber1").value);
+    var myNumber2=parseInt(document.getElementById("myNumber2").value);
+    var myUitkomst;
+
+    if(myOperator=="/"){
+        myUitkomst = myNumber1 / myNumber2;
+    }
+   
+    document.getElementById("uitkomst").innerHTML = myNumber1 + myOperator + myNumber2 + " = " + myUitkomst;
+}
+ 
