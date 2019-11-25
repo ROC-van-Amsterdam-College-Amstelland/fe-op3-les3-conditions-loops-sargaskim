911e3ceee5e24b206e854b29d7844a9121e0cf06
sargaskim <2103702@talnet.nl>
2019-11-25 13:36:35 +0100

    conditions opdr 3

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,18 @@
-//javascript code
+function getSaldo()
+{
+var saldo=document.getElementById("saldo").value;
+
+    if(saldo == 500 ) 
+        {
+        document.getElementById("comment").innerHTML = "Je salaris is gestoirt"
+        }
+    else if (saldo > 500 )
+        {
+        document.getElementById("comment").innerHTML = "Je hebt een bonus gekregen!"
+        }
+    else 
+        {    
+        document.getElementById("comment").innerHTML = "Er is geen salaris gestort"
+        }
+
+}
