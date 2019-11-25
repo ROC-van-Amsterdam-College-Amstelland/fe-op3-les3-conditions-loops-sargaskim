e91c62a5108729f6b3c7ffa6dcc73c4b6e0bb060
sargaskim <2103702@talnet.nl>
2019-11-25 13:17:42 +0100

    conditions opdr 1

--- conditions_opdrachten/conditions_opdracht_1/js/script.js
+++ conditions_opdrachten/conditions_opdracht_1/js/script.js
@@ -1,1 +1,15 @@
-//javascript code
+function getSaldo()
+{
+var saldo=document.getElementById("saldo").value;
+
+    if(saldo <=25 ) 
+        {
+        document.getElementById("comment").innerHTML = "Sorry, je hebt te weinig saldo!"
+        }
+    else if(saldo > 25 )
+        {
+        document.getElementById("comment").innerHTML = "Je hebt genoeg geld"
+
+        }
+ 
+}
