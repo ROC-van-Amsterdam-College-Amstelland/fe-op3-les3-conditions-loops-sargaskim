f0afcb2c54e84134335fcaf96b543d0ce8cf3b47
sargaskim <2103702@talnet.nl>
2019-11-29 12:03:54 +0100

    conditions opdr 7

--- conditions_opdrachten/conditions_opdracht_7/js/script.js
+++ conditions_opdrachten/conditions_opdracht_7/js/script.js
@@ -1,1 +1,19 @@
-//javascript code
+function start()
+{
+var myNumber=document.getElementById("myNumber").value;
+
+    if(myNumber <= 5){
+        document.getElementById("comment").innerHTML = "Je hebt een onveldoene"
+        document.getElementById("inputcontainer").style.backgroundColor = "red"
+    }
+
+    else if(myNumber > 5 && myNumber <= 10){
+        document.getElementById("comment").innerHTML = "Je hebt een veldoene"
+        document.getElementById("inputcontainer").style.backgroundColor = "green"
+    }
+
+    else{
+        document.getElementById("comment").innerHTML = "Er is iets fout gegaan"
+        document.getElementById("inputcontainer").style.backgroundColor = "white"
+    }
+}
