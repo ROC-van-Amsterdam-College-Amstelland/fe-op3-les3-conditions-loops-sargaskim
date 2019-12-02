8c2e533a1d40dc3d39ff420305e09202141e4d5d
sargaskim <2103702@talnet.nl>
2019-12-02 15:41:50 +0100

    loops opdr 4

--- loops_opdrachten/loops_opdracht_4/js/script.js
+++ loops_opdrachten/loops_opdracht_4/js/script.js
@@ -1,1 +1,10 @@
-//javascript code
+function go(){
+    var numbers ="";
+        for(i = 0; i <= 1000; i+=10){
+            numbers +="<br>" + i;
+        }
+    
+    document.getElementById("output1").innerHTML = numbers;
+    console.log(numbers) 
+    }
+    
