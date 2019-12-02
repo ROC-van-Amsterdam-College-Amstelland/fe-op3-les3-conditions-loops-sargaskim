66353444bc94d76638e41de09e1c11a5a7c684ed
sargaskim <2103702@talnet.nl>
2019-12-02 15:23:12 +0100

    loops opdr 3

--- loops_opdrachten/loops_opdracht_3/js/script.js
+++ loops_opdrachten/loops_opdracht_3/js/script.js
@@ -1,1 +1,15 @@
-//javascript code
+function go(){
+    var numbers ="";
+        for(i = 1; i <= 50; i++){
+            numbers +="<br>" + i;
+        
+    
+        if([i] >= 20 && [i] <= 40)
+            document.getElementById("output1").innerHTML= numbers += " Dit zijn mooie getallen";
+        
+    }
+    document.getElementById("output1").innerHTML = numbers;
+    console.log(numbers) 
+    }
+    
+    
