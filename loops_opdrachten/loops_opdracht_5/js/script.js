1ca8e29ab6d7be3ac0108b395330e70e5755660b
sargaskim <2103702@talnet.nl>
2019-12-09 13:41:23 +0100

    loops opdr 5

--- loops_opdrachten/loops_opdracht_5/js/script.js
+++ loops_opdrachten/loops_opdracht_5/js/script.js
@@ -1,1 +1,14 @@
-//javascript code
+function go(){
+    var numbers ="";
+        for(i = 1; i <= 20; i++){
+        
+
+        if (i % 2 == 1){
+        console.log(i);
+        } 
+
+        else if(i % 2 == 0) {
+        document.getElementById("output1").innerHTML += i + "<br>";
+        }
+    }
+}
