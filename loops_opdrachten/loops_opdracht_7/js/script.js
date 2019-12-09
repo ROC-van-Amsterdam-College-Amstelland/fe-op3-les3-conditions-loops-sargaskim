2f7c70bb2fa0fb91ba32641c7104bbe3ee726672
sargaskim <2103702@talnet.nl>
2019-12-09 17:57:22 +0100

    loops opdr 7 2.0

--- loops_opdrachten/loops_opdracht_7/js/script.js
+++ loops_opdrachten/loops_opdracht_7/js/script.js
@@ -1,18 +1,23 @@
 function go(){
-        var numbers ="";
-            for(i = 1; i <= 100; i++){
-        
-        if(i%3 == 0){
-            document.getElementById("output1").innerHTML += "<b>" + i + "</b>" + "<br>";    
-        }
-
-        else if(i%5 == 0){
-            document.getElementById("output1").innerHTML += i + " Cool " + "<br>";
-        }
-
-        else{
-            document.getElementById("output1").innerHTML += i + "<br>";
-        }
-    console.log(i)
+    var numbers ="";
+        for(i = 1; i <= 100; i++){
+    
+    if(i % 3 == 0 && i % 5 == 0){
+        document.getElementById("output1").innerHTML += "<b>" + i + "</b>" + " Cool " + "<br>";    
     }
+
+    else if(i % 5 == 0){
+        document.getElementById("output1").innerHTML += i + " Cool " + "<br>";
+    }
+
+    else if(i % 3 == 0){
+        document.getElementById("output1").innerHTML += "<b>" + i + "</b>" + "<br>";
+    }
+
+    else{
+        document.getElementById("output1").innerHTML += i + "<br>";
+    }
+    
+console.log(i)
+}
 }
