64ab655a1a9ea76c38b3a28e268175f85f5a7eba
sargaskim <2103702@talnet.nl>
2019-12-10 13:23:30 +0100

    loops opdr 9

--- loops_opdrachten/loops_opdracht_9/js/script.js
+++ loops_opdrachten/loops_opdracht_9/js/script.js
@@ -1,1 +1,34 @@
-//javascript code
+function go(){
+    var numbers ="";
+        for(i = 1; i <= 5; i++){
+    
+        if (i == 5){
+            document.getElementById("output1").innerHTML += i + " Het einde is bereikt!" + "<br>" + "<br>";
+            door(); 
+            break;
+        }
+
+        else{
+            document.getElementById("output1").innerHTML += i + "<br>";
+        }
+    
+    console.log(i);
+    }
+}
+function door(){
+    var numbers2 ="";
+        for(i = 5; i >= 1; i--){
+
+        if (i == 1){
+            document.getElementById("output1").innerHTML += i + " Het begin is het begin" + "<br>" + "<br>";
+            break;
+        }
+
+        else{
+            document.getElementById("output1").innerHTML += i + "<br>";
+        }
+    
+    console.log(i);
+    }
+    
+}
