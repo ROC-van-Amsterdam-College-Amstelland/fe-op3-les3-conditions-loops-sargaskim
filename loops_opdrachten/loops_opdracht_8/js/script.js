1ac081882b0a19af447830e2161dcfc60a03b11a
sargaskim <2103702@talnet.nl>
2019-12-10 12:32:45 +0100

    loops opdr 8

--- loops_opdrachten/loops_opdracht_8/js/script.js
+++ loops_opdrachten/loops_opdracht_8/js/script.js
@@ -1,1 +1,11 @@
-//javascript code
+function go(){
+    var numbers ="";
+        for(i = 1; i <= 50; i++){
+    
+        if (i == 25){
+            alert("Het getal 25 is net geen bullseye!")
+        }
+    document.getElementById("output1").innerHTML += i + "<br>"
+    }
+}
+
